74e1101913b16d505cee8d14b3c1e9bc1790f6e9
Wellington Suero <144365343+Wellfc@users.noreply.github.com>
2023-11-09 12:53:05 -0600

    Update index.js

--- index.js
+++ index.js
@@ -6,9 +6,10 @@ function select(selector) {
 }
 
 let projectName = 'Super Pizza Place';
-let address = '123 Henlow Bay';
+let address = '444 - 123 Henlow Bay';
+let postalCode = 'R3A 0H9';
 
-function printMenu() {
+function printOptions() {
     // fetch the menu
     // forEach...
     // Add the content to the index.html
